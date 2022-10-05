f4788f10a7877ff03d8ebe1edacde000c7f807d3
pnichols789 <pnichols789@gmail.com>
2022-10-05 15:58:58 -0500

    -

--- weather.js
+++ weather.js
@@ -4,5 +4,23 @@ const msg = $(".msg").eq(0);
 const list = $(".cities").eq(0);
 
 form.on("submit", (e) => {
+    e.preventDefault();
     console.log("Working");
+    getWeatherDataFromApi();
 });
+
+const getWeatherDataFromApi = () => {
+    let apiKey = "6fa14046f47a4a9f9787346b2f6a6a1c";
+    let inputVal = input.val();
+    let unit = "metric";
+    let url = `https://api/openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=${unit}`;
+    $.ajax({
+        url: url,
+        type: "GET",
+        dataType: "json",
+        success: (response) => {
+            console.log(response);
+        }
+    })
+
+};
