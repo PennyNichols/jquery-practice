33e4fec601133661a873c18bb6c78ff27653234c
pnichols789 <pnichols789@gmail.com>
2022-10-05 15:40:43 -0500

    -

--- weather.js
+++ weather.js
@@ -2,3 +2,7 @@ const form = $(".top-banner form").eq(0);
 const input = $(".top-banner input").eq(0);
 const msg = $(".msg").eq(0);
 const list = $(".cities").eq(0);
+
+form.on("submit", (e) => {
+    console.log("Working");
+});
